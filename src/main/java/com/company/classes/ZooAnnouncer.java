3503989535329139285ace330e09f0f31f42e96f
Elmer Baca Holguin <baca217@gmail.com>
2020-09-30 00:47:42 -0600

    removed uniform method, added uml pngs

--- src/main/java/com/company/classes/ZooAnnouncer.java
+++ src/main/java/com/company/classes/ZooAnnouncer.java
@@ -5,7 +5,6 @@ import java.beans.PropertyChangeListener;
 
 public class ZooAnnouncer extends ZooEmployee {
     public ZooAnnouncer(String newName){super(newName);}
-    public void uniform(){System.out.println(getName()+" puts on their Zoo Announcer uniform.");}
 
     @Override
     public void propertyChange(PropertyChangeEvent evt)
